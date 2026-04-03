4d4ead18a8ecb77eb23e69c8cbf6559553749bdf
Tyler Boutwell <tylerboutwell1@gmail.com>
2026-04-02 20:16:00 -0500

    add special_note variable for handling special notes for packages

--- package.py
+++ package.py
@@ -1,10 +1,11 @@
 class Package:
-    def __init__(self, package_id, address, deadline, city, zip_code, weight, status="At Hub"):
+    def __init__(self, package_id, address, deadline, city, zip_code, weight, special_note, status="At Hub"):
         self.package_id = package_id
         self.address = address
         self.deadline = deadline
         self.city = city
         self.zip_code = zip_code
         self.weight = weight
+        self.special_note = special_note
         self.status = status
         self.delivery_time = None  # Update when delivered
